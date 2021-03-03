3fec92c34417adbe52ae7f6d561ad5266ea68a5c
brycemooore <brycemooore@gmail.com>
2021-03-03 16:44:44 -0500

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,13 +6,14 @@ import TextLoop from 'react-text-loop';
 
 export default function Header() {
     return (
-        <div styles={{position: 'absolute'}}>
+        <div className="headerContainer">
             <video id="video" width="100%" height="100%" autoPlay={true} loop={true} muted={true}>
                 <source src={video} type="video/mp4"/>
                 Your Browser Does Not Support Video
             </video>
             <div className="overlay">
-                <img src={logo} alt="Bryce Moore" style={{height: '35vw'}}/>
+                {/* <img src={logo} alt="Bryce Moore" style={{height: '35vw'}}/> */}
+                <h1 style={{fontSize: '75px'}}>Bryce Moore</h1>
                 <h1 >
                     is a 
                 </h1>
